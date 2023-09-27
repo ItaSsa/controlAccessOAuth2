14157b27a0a2a3dd9d9599aa4bbad87ca5f9413c
Itaira <itairassa@gmail.com>
2023-09-27 16:06:01 +0200

    Spring Security filter chain not tested

--- src/main/java/com/devsuperior/demo/entities/User.java
+++ src/main/java/com/devsuperior/demo/entities/User.java
@@ -1,9 +1,16 @@
 package com.devsuperior.demo.entities;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+
+import org.apache.catalina.startup.UserDatabase;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.devsuperior.demo.projections.UserDetailsProjection;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -15,9 +22,10 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User  implements UserDetails{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -44,6 +52,13 @@ public class User {
         this.password = password;
     }
 
+    public User(UserDetailsProjection udp) {
+        id = udp.getRoleId();
+        name = udp.getUsername();
+        email = udp.getUsername();
+        password = udp.getPassword();
+    }
+    
     public Long getId() {
         return id;
     }
@@ -102,4 +117,34 @@ public class User {
     public int hashCode() {
         return id != null ? id.hashCode() : 0;
     }
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
