8987c31c692dd1d5f714c2733163bda7918f924d
Itaira <itairassa@gmail.com>
2023-09-27 11:34:09 +0200

    Bcrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$Vmq04N9j3vfmCzZGAqJZ3utb8Ibj.ffpdIepGINQKmqOmTZLZqc4y');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$Vmq04N9j3vfmCzZGAqJZ3utb8Ibj.ffpdIepGINQKmqOmTZLZqc4y');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
